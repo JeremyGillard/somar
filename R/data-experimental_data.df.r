09e618c9f6e8d13e689509341d5c0309f27a42e8
JeremyGillard <gillard.jeremy@outlook.fr>
2020-10-22 00:58:41 +0200

    addition of data

---
+++ R/data-experimental_data.df.r
@@ -0,0 +1,7 @@
+#' Gérard Derosière's experimental data.
+#'
+#' It goes without saying that I put them in the package for practical reasons but that they will be removed from the package if the package would be shared if Gérard wishes it. On the other hand if it is not so important considering the evolution of research, the dataframe could be used as an example for the use of the package.
+#'
+#' @format A Data frame of Gérard Derosière's experimental data.
+#' @source Gérard Derosière's experimental data.
+"experimental_data.df"
---
+++ R/data-somatotopy.vec.r
@@ -0,0 +1,7 @@
+#' The body area corresponds to a point on the primary somatosensory cortex.
+#'
+#' The body area corresponds to a point on the left primary somatosensory cortex starting from the bottom left in a classical pattern.
+#'
+#' @format A vector of all area.
+#' @source Wikipedia.
+"somatotopy.vec"
