6999d52288d922448bf06f482819c1d060feb07d
JeremyGillard <gillard.jeremy@outlook.fr>
2020-10-25 16:06:16 +0100

    doc(vignette): detailedguide v0.9

--- R/graph_operation.R
+++ R/graph_operation.R
@@ -52,3 +52,43 @@ layout_in_semicircle <- function(graph) {
   }
   layout_matrix
 }
+
+
+#' sequence_order_nodes_according_somatotpy
+#'
+#' Orders the nodes to reflect the real position of the areas in somatotopy.
+#'
+#' The use of this function is specific to a precise naming convention
+#' (see vignette guide) and to the field of somatotopy.
+#' sequence_order_nodes_according_somatotpy returns an order sequence of
+#' the nodes (name vector entered as a parameter) to reflect the real position
+#' of the zones in somatotopy.
+#'
+#' @param names vector. string. the names vector
+#'
+#' @importFrom stringr str_length
+#'
+#' @return returns a position sequence in order to reorder the nodes correctly.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'     sequence_order_nodes_according_somatotpy(columns names)
+#' }
+sequence_order_nodes_according_somatotpy <- function(names) {
+  prefix <- ""
+  if (grepl(names[1], pattern="^S")) {
+    prefix <- "Sel"
+  } else {
+    prefix <- "NnSel"
+  }
+  normal_names_cols <- substr(names, str_length(prefix) + 1, str_length(names))
+  f_cols <- factor(normal_names_cols, ordered = TRUE, levels = somar::somatotopy.fac)
+  sequence <- order(f_cols)
+  if (prefix == "NnSel") {
+    for (i in 1:length(sequence)) {
+      sequence[i] <- ((length(sequence) * 2) + 1) - sequence[i]
+    }
+  }
+  sequence
+}
